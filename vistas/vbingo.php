9b24b50bc2d2b2c9d72ca7cd40303024bd2edfc1
Guillermo Niebla <nieblarobertguillermo@gmail.com>
2025-10-14 11:49:47 +0200

    empezar

---
+++ vistas/vbingo.php
@@ -0,0 +1,18 @@
+<?php 
+include("../funciones/fjugadores.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$n1=test_input($_POST["jugador1"]);
+$n2=test_input($_POST["jugador2"]);
+$n3=test_input($_POST["jugador3"]);
+$n4=test_input($_POST["jugador4"]);
+
+$jugadores=array($n1=>array(),$n2=>array(),$n3=>array(),$n4=>array());
+
+rellenarCartones($jugadores);
+
+$bombo=rellenarBombo();
+
+imprimirJug($bombo,$jugadores);
+}
+?>
