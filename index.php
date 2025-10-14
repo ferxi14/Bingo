f80a89cac8cb270ceff121da3ed444d32a0387f7
ferxi14 <fernandogr0309@gmail.com>
2025-10-14 11:07:23 +0200

    corregido

--- index.php
+++ index.php
@@ -7,11 +7,22 @@
   <link rel=stylesheet href="css/estilo.css">
 </head>
 <body>
-<?php
-    include 'funciones/fjugadores.php';
+  <h1>Introduce los nombres de los 4 jugadores</h1>
 
-    rellenarArray();
+<form action="control/logica_bingo.php" method="post">
+  <label for="jugador1">Jugador 1:</label>
+  <input type="text" id="jugador1" name="jugador1" required><br><br>
 
-?>
+  <label for="jugador2">Jugador 2:</label>
+  <input type="text" id="jugador2" name="jugador2" required><br><br>
+
+  <label for="jugador3">Jugador 3:</label>
+  <input type="text" id="jugador3" name="jugador3" required><br><br>
+
+  <label for="jugador4">Jugador 4:</label>
+  <input type="text" id="jugador4" name="jugador4" required><br><br>
+
+  <button type="submit">Guardar jugadores</button>
+</form>
 </body>
 </html>
