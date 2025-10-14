f0a5c256eb439356111ad258c4947f2fea7a0b4f
GuilleNy <nieblarobertguillermo@gmail.com>
2025-10-14 17:44:26 +0200

    bingo parte del juego hecho

--- vistas/vbingo.php
+++ vistas/vbingo.php
@@ -2,17 +2,25 @@
 include("../funciones/fjugadores.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-$n1=test_input($_POST["jugador1"]);
-$n2=test_input($_POST["jugador2"]);
-$n3=test_input($_POST["jugador3"]);
-$n4=test_input($_POST["jugador4"]);
+    $n1=test_input($_POST["jugador1"]);
+    $n2=test_input($_POST["jugador2"]);
+    $n3=test_input($_POST["jugador3"]);
+    $n4=test_input($_POST["jugador4"]);
 
-$jugadores=array($n1=>array(),$n2=>array(),$n3=>array(),$n4=>array());
+    $jugadores=array($n1=>array(),$n2=>array(),$n3=>array(),$n4=>array());
 
-rellenarCartones($jugadores);
+    rellenarCartones($jugadores);
 
-$bombo=rellenarBombo();
+    $bombo=rellenarBombo();
 
-imprimirJug($bombo,$jugadores);
+
+    imprimirJug($jugadores , $bombo);
+
+
+    //Visualizar el array de jugadores
+    echo "<pre>";
+    print_r($jugadores);
+    echo "</pre>";
+    
 }
 ?>
